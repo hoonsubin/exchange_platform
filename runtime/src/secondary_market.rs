dee3da9d03410a60f6dd8a0a9c3cb647f59aa01a
Hoon Kim <hoonsubin@icloud.com>
2019-10-24 15:18:38 +0900

    added comments

--- runtime/src/secondary_market.rs
+++ runtime/src/secondary_market.rs
@@ -10,16 +10,29 @@ pub trait Trait: system::Trait {
 decl_storage! {
 	trait Store for Module<T: Trait> as SecondaryMarket {
 		Something get(something): Option<u32>;
+
+		/// The array of Accounts that have issued a share
 		IssuerArray get(issuer_array): map u64 => T::AccountId;
+
+		/// The number of share that a given Account (company) has issued
+		IssuedShares get(issued_shares): map T::AccountId => u64;
+
+		/// The state in which the Account (company) is allowed to issue shares or not
 		IsAllowedIssue get(is_allowed_issue): map T::AccountId => bool;
+
+		/// The number of shares of the company the given Account owns (parameters are Issuer, Holder)
+		OwnedShares get(owned_shares): map (T::AccountId, T::AccountId) => u64;
+
+		/// The last traded price of the given company's share
+		LastBidPrice get(last_bid_price): map T::AccountId => T::Balance;
 	}
 }
 
 decl_module! {
     /// The module declaration.
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Initializing events
-		// this is needed only if you are using events in your module
+        /// Initializing events
+		/// this is needed only if you are using events in your module
 		fn deposit_event() = default;
 
 		pub fn do_something(origin, something: u32) -> Result {
@@ -27,6 +40,13 @@ decl_module! {
 
 			Something::put(something);
 			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			Ok(())
+		}
+
+		pub fn allow_issue(origin, firm: T::AccountId) -> Result {
+			let sender = ensure_signed(origin)?;
+			
+
 			Ok(())
 		}
 
