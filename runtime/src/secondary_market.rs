d11d2b2786e528f485c395c02d9f737b10ef0350
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2019-10-25 19:20:28 +0900

    added share issuing function and more
    you can add shares, retire shares. And there are some name changes to variables

--- runtime/src/secondary_market.rs
+++ runtime/src/secondary_market.rs
@@ -13,8 +13,8 @@ decl_storage! {
 		/// This will only be populated when the Account starts to issue shares
 		IssuerArray get(issuer_array): map u64 => T::AccountId;
 
-		/// The number of share that a given Account (company) has issued
-		TotalIssuedShares get(total_issued_shares): map T::AccountId => u64;
+		/// The number of share that a given Account (company) has issued, and can be traded
+		FloatingShares get(floating_shares): map T::AccountId => u64;
 
 		/// The state in which the Account (company) is allowed to issue shares or not
 		IsAllowedIssue get(is_allowed_issue): map T::AccountId => bool = false;
@@ -26,7 +26,7 @@ decl_storage! {
 		LastBidPrice get(last_bid_price): map T::AccountId => T::Balance;
 
 		/// The maximum shares the given company can issue
-		MaxShare get(max_share): map T::AccountId => u64;
+		AuthorizedShares get(authorized_shares): map T::AccountId => u64;
 
 		/// The market freeze state. Making this true will stop all further exchange
 		MarketFreeze get(market_freeze): bool = false;
@@ -44,14 +44,14 @@ decl_module! {
 			ensure!(sender != firm, "you cannot give rights to yourself");
 			ensure!(Self::is_allowed_issue(&firm) == false, "the firm is already allowed to issue shares");
 
-			let current_share_lim = Self::max_share(&firm);
+			let current_share_lim = Self::authorized_shares(&firm);
 
 			// only add the given share limit when the current limit is 0
 			if current_share_lim == 0 {
 				// make sure the new limit value is more than 0
 				ensure!(share_limit > 0, "the value must be greater than 0");
 
-				<MaxShare<T>>::insert(firm.clone(), share_limit);
+				<AuthorizedShares<T>>::insert(firm.clone(), share_limit);
 			}
 
 			<IsAllowedIssue<T>>::insert(firm.clone(), true);
@@ -74,17 +74,63 @@ decl_module! {
 			Ok(())
 		}
 
-		pub fn change_share_limit(origin, firm: T::AccountId, new_limit: u64) -> Result {
+		pub fn change_authorized_shares(origin, firm: T::AccountId, new_limit: u64) -> Result {
 			// todo: make this ensure that origin is root
 			let sender = ensure_signed(origin)?;
 			ensure!(<IsAllowedIssue<T>>::get(&firm), "the firm is not allowed to issue shares");
 			ensure!(sender != firm.clone(), "you cannot change your own issue limit");
-			ensure!(new_limit > Self::total_issued_shares(&firm), "the firm cannot limit shares \
+			ensure!(new_limit > Self::floating_shares(&firm), "the firm cannot limit shares \
 				less than the already issued amount");
 
-			<MaxShare<T>>::insert(firm.clone(), new_limit);
+			<AuthorizedShares<T>>::insert(firm.clone(), new_limit);
 
-			Self::deposit_event(RawEvent::ChangedIssueLimit(firm, new_limit));
+			Self::deposit_event(RawEvent::AdjustedAuthorizedStock(firm, new_limit));
+
+			Ok(())
+		}
+
+		pub fn issue_shares(origin, amount: u64) -> Result {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Self::is_allowed_issue(&sender), "this firm is not allowed to issue additional shares");
+
+			let old_shares_outstanding = Self::floating_shares(&sender);
+
+			//let new_shares_outstanding = Self::floating_shares(&sender) + amount.clone();
+			let new_shares_outstanding = old_shares_outstanding.checked_add(amount.clone())
+				.ok_or("overflowing when issuing new shares")?;
+
+			ensure!(new_shares_outstanding < Self::authorized_shares(&sender), "already issued the maximum amount of shares");
+
+			<FloatingShares<T>>::insert(&sender, new_shares_outstanding);
+			<OwnedShares<T>>::insert((sender.clone(), sender.clone()), amount.clone());
+
+			Self::deposit_event(RawEvent::IssuedShares(sender, amount));
+
+			Ok(())
+		}
+
+		pub fn retire_shares(origin, amount: u64) -> Result {
+			let sender = ensure_signed(origin)?;
+
+			// get the caller's own shares
+			let held_shares = Self::owned_shares((sender.clone(), sender.clone()));
+
+			// prevent underflow by making sure that the account has more shares than the amount to decrease
+			ensure!(held_shares >= amount, "you do not have enough shares to retire");
+
+			// the new number of shares the caller will have
+			//let new_amount = held_shares - amount;
+			let new_amount = held_shares.checked_sub(amount).ok_or("underflow while subtracting held shares")?;
+
+			// the new number of floating shares that is issued by the caller
+			//let new_float = Self::floating_shares(&sender) - amount;
+			let new_float = Self::floating_shares(&sender).checked_sub(amount).ok_or("underflow while subtracting floating shares")?;
+
+			<OwnedShares<T>>::insert((sender.clone(), sender.clone()), new_amount);
+			<FloatingShares<T>>::insert(sender.clone(), new_float);
+
+			Self::deposit_event(RawEvent::RetiredShares(sender, amount));
 
 			Ok(())
 		}
@@ -121,7 +167,11 @@ decl_event!(
 		GaveIssueRight(AccountId, u64),
 		RevokedIssueRight(AccountId),
 		MarketFrozen(AccountId, bool),
-		ChangedIssueLimit(AccountId, u64),
+		AdjustedAuthorizedStock(AccountId, u64),
+		IssuedShares(AccountId, u64),
+		RetiredShares(AccountId, u64),
+
+
 	}
 );
 
