464303e4cb1edc1f2827fd6921a43518d4e032bc
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2019-11-15 11:49:34 +0900

    changed transaction fees

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -110,9 +110,9 @@ fn testnet_genesis(initial_authorities: Vec<AuthorityId>, endowed_accounts: Vec<
 		}),
 		balances: Some(BalancesConfig {
 			transaction_base_fee: 1,
-			transaction_byte_fee: 0,
+			transaction_byte_fee: 1,
 			existential_deposit: 500,
-			transfer_fee: 0,
+			transfer_fee: 5,
 			creation_fee: 0,
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 			vesting: vec![],
